d4316c45df86c612717f9e59e5114293cf7867c0
Francesc Rocher <francesc.rocher@gmail.com>
2023-01-14 21:01:21 +0100

    Add calendar package

---
+++ src/euler_calendar.adb
@@ -0,0 +1,36 @@
+--  ---------------------------------------------------------------------------
+--
+--  Copyright (c) 2023 Francesc Rocher <francesc.rocher@gnail.com>
+--  SPDX-License-Identifier: CC-BY-NC-SA-4.0
+--  https://creativecommons.org/licenses/by-nc-sa/4.0/
+--
+--  ---------------------------------------------------------------------------
+--   _____      _             _____           _
+--  | ____|   _| | ___ _ __  |_   _|__   ___ | |___   Mathematical functions
+--  |  _|| | | | |/ _ \ '__|   | |/ _ \ / _ \| / __|  and tools to solve
+--  | |__| |_| | |  __/ |      | | (_) | (_) | \__ \  Project Euler problems
+--  |_____\__,_|_|\___|_|      |_|\___/ \___/|_|___/  https://projecteuler.net
+--
+-- ----------------------------------------------------------------------------
+
+package body Euler_Calendar is
+
+   function Weekday
+     (Year : Natural; Month : Month_Type; Day : Natural) return Day_Type
+   is
+      Y : Natural          := Year;
+      M : constant Natural := Month_Type'Enum_Rep (Month);
+      D : Natural          := Day;
+      E : Natural;
+   begin
+      if Month < Mar then
+         D := @ + Y;
+         Y := @ - 1;
+      else
+         D := @ + Y - 2;
+      end if;
+      E := ((23 * M / 9) + D + 4 + (Y / 4) - (Y / 100) + (Y / 400)) mod 7;
+      return Day_Type'Enum_Val (E);
+   end Weekday;
+
+end Euler_Calendar;
---
+++ src/euler_calendar.ads
@@ -0,0 +1,32 @@
+--  ---------------------------------------------------------------------------
+--
+--  Copyright (c) 2023 Francesc Rocher <francesc.rocher@gnail.com>
+--  SPDX-License-Identifier: CC-BY-NC-SA-4.0
+--  https://creativecommons.org/licenses/by-nc-sa/4.0/
+--
+--  ---------------------------------------------------------------------------
+--   _____      _             _____           _
+--  | ____|   _| | ___ _ __  |_   _|__   ___ | |___   Mathematical functions
+--  |  _|| | | | |/ _ \ '__|   | |/ _ \ / _ \| / __|  and tools to solve
+--  | |__| |_| | |  __/ |      | | (_) | (_) | \__ \  Project Euler problems
+--  |_____\__,_|_|\___|_|      |_|\___/ \___/|_|___/  https://projecteuler.net
+--
+-- ----------------------------------------------------------------------------
+
+package Euler_Calendar is
+
+   type Month_Type is
+     (Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec);
+   for Month_Type use
+     (Jan => 1, Feb => 2, Mar => 3, Apr => 4, May => 5, Jun => 6, Jul => 7,
+      Aug => 8, Sep => 9, Oct => 10, Nov => 11, Dec => 12);
+
+   type Day_Type is (Sun, Mon, Tue, Wed, Thu, Fri, Sat);
+   for Day_Type use
+     (Sun => 0, Mon => 1, Tue => 2, Wed => 3, Thu => 4, Fri => 5, Sat => 6);
+
+   function Weekday
+     (Year : Natural; Month : Month_Type; Day : Natural) return Day_Type;
+   --  Returns the day of the week of Year, Month Day.
+
+end Euler_Calendar;
